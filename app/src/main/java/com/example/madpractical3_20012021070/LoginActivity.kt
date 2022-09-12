11a4afbd7fc8bee850cf9f570a20631cd1c172e4
DARSHAN <darshanvasoya@gnu.ac.in>
2022-09-13 00:15:24 +0530

    practical-3 completed

--- app/src/main/java/com/example/madpractical3_20012021070/LoginActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012021070/LoginActivity.kt
@@ -17,3 +17,5 @@ class LoginActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
     }
 }
+
+
--- app/src/main/java/com/example/madpractical3_20012021070/MainActivity.kt
+++ app/src/main/java/com/example/madpractical3_20012021070/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.madpractical3_20012021070
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.view.WindowCompat
@@ -10,9 +11,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         super.onCreate(savedInstanceState)
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setSupportActionBar(binding.toolbar)
+
+       /* binding.BottomNavigationView.id = R.id.bottom_nav_reg
+        binding.BottomNavigationView.setOnItemSelectedListener { it ->
+            when(it.itemId){
+                R.id.bottom_nav_reg -> {
+                    Intent(this, RegistrationActivity::class.java).also { startActivity(it) }
+                }
+                return@setOnItemSelectedListener true
+            }
+        }*/
+
+        binding.BottomNavigationView.id = R.id.bottom_nav_login
+        binding.BottomNavigationView.setOnItemSelectedListener { it1 ->
+            when (it1.itemId) {
+                R.id.bottom_nav_login -> {
+                    Intent(this, LoginActivity::class.java).also { startActivity(it) }
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
